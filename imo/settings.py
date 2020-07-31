3065f727f0ee48d3ca7486a2f979e69567c6827b
Srijay <parsi.srijay@gmail.com>
2020-07-31 09:15:48 +0530

    dep

--- imo/settings.py
+++ imo/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'w)%9tpayz=29btzyb40k8hwahzzxv=@sr%k0-qf-ddj4y_q+lo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://sihimo.herokuapp.com','127.0.0.1']
 
 
 # Application definition
